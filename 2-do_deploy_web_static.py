d837594d200a8f30e3eafe45f00b6a8124425dc5
asmarais <rais.asma99@gmail.com>
2023-10-05 14:20:11 +0100

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from fabric.api import env, put, run, sudo
 from os.path import exists, basename, splitext
-
 # Define the remote servers
 env.hosts = ['100.25.188.65', '34.204.61.68']
+
 
 def do_deploy(archive_path):
     """
@@ -16,11 +16,14 @@ def do_deploy(archive_path):
         filename = splitext(basename(archive_path))[0]
         put(archive_path, '/tmp/')
         run('mkdir -p /data/web_static/releases/{}/'.format(filename))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename + '.tgz', filename))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.
+            format(filename + '.tgz', filename))
         run('rm /tmp/{}'.format(filename + '.tgz'))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(filename, filename))
+        run('mv /data/web_static/releases/{}/web_static/*'
+            '/data/web_static/releases/{}/'.format(filename, filename))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
+            format(filename))
         return True
 
     except Exception as e:
