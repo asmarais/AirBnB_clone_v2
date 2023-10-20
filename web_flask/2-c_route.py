930cbe8beac942be2c0592361876178acf180064
asmarais <rais.asma99@gmail.com>
2023-10-20 11:11:22 +0100

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,21 +4,26 @@
 
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def home():
     """ Returns Hello HBNB"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Returns HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """ replace text with variable. """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
